afde3c75e8ae91f0b2df6d1e636d43e6f43f6d85
Jan Wosnitza <jan_wosnitza@hotmail.com>
2021-05-16 10:55:02 +0200

    undo SDFs as structs

--- src/FrayTracer/SDF.fs
+++ src/FrayTracer/SDF.fs
@@ -6,38 +6,7 @@ open FrayTracer
 type ISignedDistanceField =
     abstract GetDistance : position:Vector3 -> float32
 
-module Helper =
-    let private bindingFlags =
-        Reflection.BindingFlags.Instance
-        ||| Reflection.BindingFlags.Public
-        ||| Reflection.BindingFlags.NonPublic
-
-    let construct<'Type when 'Type : struct> (parameters:obj[]) =
-        let t = typedefof<'Type>
-
-        let getCtor (t:Type) =
-            let parameterCount = parameters.Length
-            t.GetConstructors(bindingFlags)
-            |> Seq.find (fun x -> x.GetParameters().Length = parameterCount)
-
-        let ctor = getCtor t
-
-        let genericParameters = t.GetGenericArguments()
-        (ctor.GetParameters(), parameters)
-        ||> Seq.iter2 (fun param value ->
-            if param.ParameterType.IsGenericParameter then
-                genericParameters.[param.ParameterType.GenericParameterPosition] <- value.GetType()
-            )
-
-        let ctor = getCtor (t.MakeGenericType(genericParameters))
-        ctor.Invoke(parameters)
-
-    let construct'<'Type when 'Type : struct> (parameters:obj[]) =
-        let ctor = typeof<'Type>.GetConstructors(bindingFlags).[0]
-        ctor.Invoke(parameters)
-
 module Primitive =
-    [<Struct>]
     type Sphere =
         {
             Center : Vector3
@@ -46,206 +15,95 @@ module Primitive =
 
         interface ISignedDistanceField with
             member this.GetDistance(position) =
-                //let x = position.X - this.Center.X
-                //let y = position.Y - this.Center.Y
-                //let z = position.Z - this.Center.Z
-                //MathF.Sqrt(x * x + y * y + z * z) - this.Radius
                 Vector3.Distance(this.Center, position) - this.Radius
 
     let sphere (x:Sphere) = x  :> ISignedDistanceField
 
 module Combine =
-    [<Struct>]
-    type private Union<'a, 'b
-        when 'a :> ISignedDistanceField
-        and 'b :> ISignedDistanceField
-        > =
-        {
-            mutable A:'a
-            mutable B:'b
-        }
-
-        interface ISignedDistanceField with
+    let union (a:ISignedDistanceField) (b:ISignedDistanceField) =
+        {new ISignedDistanceField with
             member this.GetDistance(position) =
                 MathF.Min(
-                    this.A.GetDistance(position),
-                    this.B.GetDistance(position)
+                    a.GetDistance(position),
+                    b.GetDistance(position)
                 )
-
-    let union (a:ISignedDistanceField) (b:ISignedDistanceField) =
-        Helper.construct<Union<_, _>> [|
-            box a
-            box b
-        |] :?> ISignedDistanceField
-
-    [<Struct>]
-    type private Intersection<'a, 'b
-        when 'a :> ISignedDistanceField
-        and 'b :> ISignedDistanceField
-        > =
-        {
-            mutable A:'a
-            mutable B:'b
         }
-        interface ISignedDistanceField with
-            member this.GetDistance(position) =
-                MathF.Max(
-                    this.A.GetDistance(position),
-                    this.B.GetDistance(position)
-                )
 
     let intersection (a:ISignedDistanceField) (b:ISignedDistanceField) =
-        Helper.construct<Intersection<_, _>> [|
-            box a
-            box b
-        |] :?> ISignedDistanceField
-
-    [<Struct>]
-    type private Subtraction<'a, 'b
-        when 'a :> ISignedDistanceField
-        and 'b :> ISignedDistanceField
-        > =
-        {
-            mutable A:'a
-            mutable B:'b
-        }
-        interface ISignedDistanceField with
+        {new ISignedDistanceField with
             member this.GetDistance(position) =
                 MathF.Max(
-                    this.A.GetDistance(position),
-                    -this.B.GetDistance(position)
+                    a.GetDistance(position),
+                    b.GetDistance(position)
                 )
+        }
 
     let subtraction (a:ISignedDistanceField) (b:ISignedDistanceField) =
-        Helper.construct<Subtraction<_, _>> [|
-            box a
-            box b
-        |] :?> ISignedDistanceField
-
-    type private IUnionSmoothSum =
-        abstract Get : strength:float32 * position:Vector3 -> float32
-
-    [<Struct>]
-    type private UnionSmoothSumSingle<'sdf when 'sdf :> ISignedDistanceField> =
-        {
-        mutable Sdf : 'sdf
-        }
-
-        interface IUnionSmoothSum with
-            member this.Get(strength, position) =
-                MathF.Exp(-strength * this.Sdf.GetDistance(position))
-
-    [<Struct>]
-    type private UnionSmoothSumCombine<'sum, 'sdf
-        when 'sum :> IUnionSmoothSum
-        and 'sdf :> ISignedDistanceField> =
-        {
-        mutable Sum : 'sum
-        mutable Sdf : 'sdf
-        }
-
-        interface IUnionSmoothSum with
-            member this.Get(strength, position) =
-                this.Sum.Get(strength, position) + MathF.Exp(-strength * this.Sdf.GetDistance(position))
-
-    [<Struct>]
-    type private UnionSmooth<'sum
-        when 'sum :> IUnionSmoothSum
-        > =
-        {
-            Strength : float32
-            mutable Sum : 'sum
-        }
-
-        interface ISignedDistanceField with
+        {new ISignedDistanceField with
             member this.GetDistance(position) =
-                float32 <| (-MathF.Log(this.Sum.Get(this.Strength, position)) / this.Strength)
+                MathF.Max(
+                    a.GetDistance(position),
+                    -b.GetDistance(position)
+                )
+        }
 
     let unionSmooth (strength:float32) (sdfs:ISignedDistanceField list) =
         match sdfs with
         | [] -> failwithf "blub"
         | [sdf] -> sdf
-        | sdf :: sdfs ->
-            let sum =
-                (Helper.construct<UnionSmoothSumSingle<_>> [|box sdf|] :?> IUnionSmoothSum, sdfs)
-                ||> List.fold (fun sum sdf -> Helper.construct<UnionSmoothSumCombine<_, _>> [|box sum; box sdf|] :?> IUnionSmoothSum)
+        | _ ->
+            let sdfs = sdfs |> List.toArray
 
-            Helper.construct<UnionSmooth<_>> [|
-                box strength
-                box sum
-            |] :?> ISignedDistanceField
+            {new ISignedDistanceField with
+                member this.GetDistance(position) =
+                    let mutable sum = 0f
+                    for i = 0 to sdfs.Length - 1 do
+                        sum <- sum + MathF.Exp(-strength * sdfs.[i].GetDistance(position))
 
-    [<Struct>]
-    type private Cache<'sdf
-        when 'sdf :> ISignedDistanceField
-        > =
-        {
-            Width : float32
-            HalfDiagonal : float32
-            Epsilon : float32
-            CachedDistances : System.Collections.Concurrent.ConcurrentDictionary<struct (int * int * int), float32>
-            mutable Sdf : 'sdf
-        }
+                    -MathF.Log(sum) / strength
+            }
 
-        interface ISignedDistanceField with
+    let cache (width:float32) (epsilon:float32) (sdf:ISignedDistanceField) =
+        let halfDiagonal = sqrt (width * width * 3f) * 0.5f
+        let cachedDistances = System.Collections.Concurrent.ConcurrentDictionary<struct (int * int * int), float32>()
+        {new ISignedDistanceField with
             member this.GetDistance(position) =
-                let width = this.Width
+                let width = width
                 let x = MathF.roundToInt (position.X / width)
                 let y = MathF.roundToInt (position.Y / width)
                 let z = MathF.roundToInt (position.Z / width)
 
                 let optDistance =
                     let pos = struct (x, y, z)
-                    match this.CachedDistances.TryGetValue(pos) with
+                    match cachedDistances.TryGetValue(pos) with
                     | true, optDistance -> optDistance
                     | _ ->
                         let distance =
-                            this.Sdf.GetDistance(Vector3(float32 x * width, float32 y * width, float32 z * width))
-                            - this.HalfDiagonal
+                            sdf.GetDistance(Vector3(float32 x * width, float32 y * width, float32 z * width))
+                            - halfDiagonal
                         
                         let optDistance =
-                            if distance > this.Epsilon
+                            if distance > epsilon
                             then distance
                             else Single.NaN
-                        this.CachedDistances.TryAdd(pos, optDistance) |> ignore
+                        cachedDistances.TryAdd(pos, optDistance) |> ignore
                         optDistance
 
                 if Single.IsNaN(optDistance) then
-                    this.Sdf.GetDistance(position)
+                    sdf.GetDistance(position)
                 else
                     optDistance
-
-    let cache (width:float32) (epsilon:float32) (sdf:ISignedDistanceField) =
-            Helper.construct<Cache<_>> [|
-                box width
-                box (sqrt (width * width * 3f) * 0.5f)
-                box epsilon
-                box (System.Collections.Concurrent.ConcurrentDictionary<struct (int * int * int), float32>())
-                box sdf
-            |] :?> ISignedDistanceField
-
-    [<Struct>]
-    type private Measure<'sdf
-        when 'sdf :> ISignedDistanceField
-        > =
-        {
-            Stopwatch : System.Diagnostics.Stopwatch
-            mutable Sdf : 'sdf
         }
 
-        interface ISignedDistanceField with
+    let measure (stopwatch:System.Diagnostics.Stopwatch) (sdf:ISignedDistanceField) =
+        {new ISignedDistanceField with
             member this.GetDistance(position) =
-                this.Stopwatch.Start()
+                stopwatch.Start()
                 try
-                    this.Sdf.GetDistance(position)
+                    sdf.GetDistance(position)
                 finally
-                    this.Stopwatch.Stop()
-
-    let measure (stopwatch) (sdf:ISignedDistanceField) =
-            Helper.construct<Measure<_>> [|
-                box stopwatch
-                box sdf
-            |] :?> ISignedDistanceField
+                    stopwatch.Stop()
+        }
 
 module Test =
     let trace (epsilon:float32) (length:float32) (sdf:ISignedDistanceField) (ray:Ray) =
